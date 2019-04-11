07f8119327e4762b24725529b9a94d491fee46d9
squteronsquter <squter@squter.pl>
2019-04-11 12:30:00 +0200

    webpack config appended

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.css',
+  entry: ['./src/app.css', './src/main.js'],
   mode: process.env.NODE_ENV,
   module: {
     rules: [
